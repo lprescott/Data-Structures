387bb5dc7632976cd889f90dc881a0c02d9526ee
Luke Prescott <lprescott@users.noreply.github.com>
2017-05-24 16:14:53 -0400

    Add files via upload

---
+++ AcademicPrescottLukeH1/README.md
@@ -0,0 +1,1 @@
+# PrescottLukeH6
---
+++ AcademicPrescottLukeH1/src/binaryTree.java
@@ -0,0 +1,13 @@
+//Luke R. Prescott
+//H6 Trees
+
+public class binaryTree 
+{
+	protected Node root;
+	
+	//isEmpty method returns true if Tree is empty
+	public boolean isEmpty()
+	{
+		return root == null;
+	}
+}
